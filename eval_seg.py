054a9ccd27c5f13eaf6d4cdaaf95c514dd8aa238
siddharthdesai06 <siddharthdesai0602@gmail.com>
2025-03-26 11:38:09 +0530

    evaluation on ramen

--- eval_seg.py
+++ eval_seg.py
@@ -459,7 +459,7 @@ def main(
     rasterizer: Literal[
     "inria", "gsplat"
     ] = "inria",  # Original or gsplat for checkpoints
-    prompt: str = "dining table", # the one to be extracted or deleted
+    prompt: str = "cake", # the one to be extracted or deleted
     data_factor: int = 4,
     show_visual_feedback: bool = True,
 ):
