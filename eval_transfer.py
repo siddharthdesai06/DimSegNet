86e835925383a1c1ec02af6f80d77a1caed27415
siddharthdesai06 <siddharthdesai0602@gmail.com>
2025-04-07 13:08:12 +0530

    dim reduction

--- eval_transfer.py
+++ eval_transfer.py
@@ -560,7 +560,7 @@ def create_feature_field_yolo_sam_clip(splats, sam_checkpoint, clip_embeddings_p
 
             image_original = cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR)  
             print("Final Feats shape", feats.shape)
-            feats_flattened = feats.reshape(-1, 512).detach().cpu().numpy()
+            feats_flattened = feats.reshape(-1, embed_dim).detach().cpu().numpy()
             if first_time:
                 feats_pca = pca.fit_transform(feats_flattened)
                 feats_pca_first_time = feats_pca
@@ -663,9 +663,9 @@ def create_feature_field_yolo_sam_clip(splats, sam_checkpoint, clip_embeddings_p
 
 def main(
     
-    data_dir: str = "/home/siddharth/siddharth/thesis/Yolo_segmentation/eval_datasets/figurines",  # colmap path
-    checkpoint: str = "/home/siddharth/siddharth/thesis/Yolo_segmentation/eval_datasets/figurines/chkpnt30000.pth",  # checkpoint path, can generate from original 3DGS repo
-    results_dir: str = "./results/figurines",
+    data_dir: str = "/home/siddharth/siddharth/thesis/Yolo_segmentation/eval_datasets/teatime",  # colmap path
+    checkpoint: str = "/home/siddharth/siddharth/thesis/Yolo_segmentation/eval_datasets/teatime/chkpnt30000.pth",  # checkpoint path, can generate from original 3DGS repo
+    results_dir: str = "./results/teatime",
 
     # data_dir: str = "/home/siddharth/siddharth/thesis/3dgs-gradient-backprojection/data/garden",  # colmap path
     # checkpoint: str = "/home/siddharth/siddharth/thesis/3dgs-gradient-backprojection/data/garden/ckpts/ckpt_29999_rank0.pt",  # checkpoint path, can generate from original 3DGS repo
@@ -678,7 +678,7 @@ def main(
     ] = "inria",  # Original or GSplat for checkpoints
     data_factor: int = 4,
     embed_dim: int=16,
-    compress: bool=False,
+    compress: bool=True,
    
 ):
     test_images = {"test_0.jpg", "test_1.jpg", "test_2.jpg", "test_3.jpg", "frame_00131.jpg"} 
