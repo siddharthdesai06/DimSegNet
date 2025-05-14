0891d4d7d5658a2cd91f9fc87e9b9348d5f118ac
siddharthdesai06 <siddharthdesai0602@gmail.com>
2025-05-14 13:40:12 +0530

    eval replicaoffice0 64 dim

--- segment_slam_compressed.py
+++ segment_slam_compressed.py
@@ -314,11 +314,11 @@ def get_mask3d_yolo(splats, gaussian_features, prompt, neg_prompt, threshold=Non
     text_feat_norm = torch.nn.functional.normalize(text_feat, p=2, dim=1)
     
     # Compress text prompts 512->16
-    text_feat_compressed = text_feat_norm@encoder_decoder.encoder # 512 -> 16
-    text_feat = torch.nn.functional.normalize(text_feat_compressed,p=2,dim=1)
+    # text_feat_compressed = text_feat_norm@encoder_decoder.encoder # 512 -> 16
+    # text_feat_norm = torch.nn.functional.normalize(text_feat_compressed,p=2,dim=1)
 
     # Compute similarity scores
-    score = gaussian_features @ text_feat.T
+    score = gaussian_features @ text_feat_norm.T
     
     # Compute masks
     mask_3d = score[:, 0] > score[:, 1:].max(dim=1)[0]
@@ -485,7 +485,7 @@ def main(
     rasterizer: Literal[
         "inria", "gsplat"
     ] = "inria",  # Original or gsplat for checkpoints
-    prompt: str = "chair", # the one to be extracted or deleted
+    prompt: str = "dining table", # the one to be extracted or deleted
     data_factor: int = 4,
     show_visual_feedback: bool = True,
 
